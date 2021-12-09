29fabd73faa7c978cc9a9213d35505b8a647c882
Patrick <leepc777@live.com>
2021-12-09 15:40:08 -0800

    initial tableView is completed

--- FlickrSearch/ViewController.swift
+++ FlickrSearch/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    var imageURLStrings = ["a","b","c"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.rowHeight = self.view.frame.size.height/4
+//        tableView.reloadData()
     }
 
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageURLStrings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "aCell", for: indexPath)
+        
+        cell.textLabel?.text = imageURLStrings[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(" you tapped tableView cell at indexPath:\(indexPath)")
+    }
+    
 }
 
